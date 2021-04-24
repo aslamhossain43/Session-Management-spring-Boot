e6a93ee5bc179e7cd05d1af10029b89dd9a2467d
Aslam-Hossain <aslamecehstu43java@gmail.com>
2021-04-24 23:38:31 +0600

    Session with redis is added

---
+++ Session-Redis/src/test/java/com/session/main/SessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
